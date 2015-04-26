f5831f2ec9ccfc04f1b730096a9365c79784a0f9
kawamura.hryk <kawamura.hryk@gmail.com>
2015-04-26 15:42:50 +0900

    Window 3分割

--- inits/linux-appearance.el
+++ inits/linux-appearance.el
@@ -6,8 +6,8 @@
 ;;                   '("Takaoゴシック" . "unicode-bmp")
 ;;                   )
 
-(set-default-font "Yutapon coding Bold-10.5")
-(set-face-font 'variable-pitch "Yutapon coding Regular-10.5")
+(set-default-font "Yutapon coding Bold-14.0")
+(set-face-font 'variable-pitch "Yutapon coding Regular-14.0")
 (set-fontset-font (frame-parameter nil 'font)
                   'japanese-jisx0208
                   '("Yutapon coding Bold" . "unicode-bmp")
