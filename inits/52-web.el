0c8ebee52d33a833e209e7a1d34810e5931f0e9c
kawamura.hryk <kawamura.hryk@gmail.com>
2015-06-19 19:35:14 +0900

    Modify web-mode

--- inits/52-web.el
+++ inits/52-web.el
@@ -7,9 +7,21 @@
 (add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
 
-(defun web-mode-hook ()
-  "Hooks for Web mode."
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 2))
-(add-hook 'web-mode-hook 'web-mode-hook)
+;; (defun web-mode-hook ()
+;;   "Hooks for Web mode."
+;;   (setq web-mode-markup-indent-offset 2)
+;;   (setq web-mode-css-indent-offset 2)
+;;   (setq web-mode-code-indent-offset 2))
+;; (add-hook 'web-mode-hook 'web-mode-hook)
+
+;;; @link http://cortyuming.hateblo.jp/entry/20140904/p2
+(defun web-mode-indent (num)
+  (interactive "nIndent: ")
+  (setq web-mode-markup-indent-offset num)
+  (setq web-mode-css-indent-offset num)
+  (setq web-mode-style-padding num)
+  (setq web-mode-code-indent-offset num)
+  (setq web-mode-script-padding num)
+  (setq web-mode-block-padding num)
+  )
+(web-mode-indent 2)
